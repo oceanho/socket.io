5c031b2dbd890ca92f41149f15081c580525e506
Ocean <gzhehai@foxmail.com>
2020-10-22 21:10:19 +0800

    Add redis-io Adapter

--- examples/whiteboard/index.js
+++ examples/whiteboard/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const redis = require('socket.io-redis');
 const port = process.env.PORT || 3000;
 
+const redis_host = process.env.REDIS_HOST || 'localhost';
+const redis_port = process.env.REDIS_PORT || 6379;
+const redis_options = { host: redis_host, port: redis_port }
+io.adapter(redis(redis_options))
 app.use(express.static(__dirname + '/public'));
 
 function onConnection(socket){
